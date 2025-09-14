5a99f88d046a0f12285461192adb01720439d575
siba-x-prasad <siba.x.prasad@gmail.com>
2025-09-14 18:21:26 +0100

    Initialize IOS project using Swift UI

---
+++ IosPlayground/IosPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosPlaygroundApp.swift
+//  IosPlayground
+//
+//  Created by Sibaprasad Mohanty on 14/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct IosPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IosPlaygroundTests/IosPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  IosPlaygroundTests.swift
+//  IosPlaygroundTests
+//
+//  Created by Sibaprasad Mohanty on 14/09/2025.
+//
+
+import Testing
+@testable import IosPlayground
+
+struct IosPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
